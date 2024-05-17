ff927e0e2196a3599d1a53b38f4ac9f4c7e815c7
Ashish Rajwar <rajwarashish142@gmail.com>
2024-05-17 17:01:01 +0530

    transport added

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -81,7 +81,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
     }
   };
   useEffect(() => {
-    socket = io(END_POINT);
+    socket = io(END_POINT, { transports: ["websocket"] });
     socket.emit("setup", user);
     socket.on("connected", () => setSocketConnected(true));
     socket.on("typing", () => setIsTyping(true));
